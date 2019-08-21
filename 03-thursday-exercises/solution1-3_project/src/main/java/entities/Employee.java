50b4b5fdb3918150a54ca37b319bdc8f453a3c28
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-08-21 21:56:35 +0200

    Employee dto created

--- 03-thursday-exercises/solution1-3_project/src/main/java/entities/Employee.java
+++ 03-thursday-exercises/solution1-3_project/src/main/java/entities/Employee.java
@@ -1,23 +1,32 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
-public class RenameMe implements Serializable {
+public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String address;
+    private float salary;
 
-    public RenameMe() {
+    public Employee() {
     }
-        
+
+    public Employee(String name, String address, float salary) {
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
     public Long getId() {
         return id;
     }
@@ -25,5 +34,72 @@ public class RenameMe implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Enployee{" + "id=" + id + ", name=" + name + ", address=" + address + ", salary=" + salary + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.address);
+        hash = 59 * hash + Float.floatToIntBits(this.salary);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Employee other = (Employee) obj;
+        if (Float.floatToIntBits(this.salary) != Float.floatToIntBits(other.salary)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
     
 }
